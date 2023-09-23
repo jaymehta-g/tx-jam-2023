28933eda1663d486ef66f705e42a3879ea9bf309
Jay Mehta Laptop <wjayww+asus_laptop@gmail.com>
2023-09-23 15:38:44 -0500

    start work on trap enable animation

--- entity/player/states/place_trap.gd
+++ entity/player/states/place_trap.gd
@@ -1,18 +1,27 @@
 extends State
 
-@export var bouncer_shape_cast: ShapeCast2D
+@export var bouncer_shape_cast: Area2D
 
 var player: Player
+
+var bouncer_is_colliding := false
 
 func state_enter() -> void:
 	player = base
 	if player.held_trap_type == TrapType.BOUNCER:
 		var node := ResourceManager.BUMPER_SCENE.instantiate()
-		var position: Vector2
-		if bouncer_shape_cast.is_colliding():
-			position = bouncer_shape_cast.get_collision_point(0)
-		else:
-			position = player.position + bouncer_shape_cast.target_position * player.facing_right_multiplier
-		node.position = position
-		player.add_to_level.emit(node)
+		if not bouncer_is_colliding:
+			node.position = bouncer_shape_cast.position
+			node.position += player.position 
+			player.add_to_level.emit(node)
 	transition_state("move")
+
+func _process(delta: float) -> void:
+	bouncer_shape_cast.position.x = abs(bouncer_shape_cast.position.x) * base.facing_right_multiplier
+
+func _on_bouncer_placement_body_exited(body:Node2D) -> void:
+	bouncer_is_colliding = false
+
+
+func _on_bouncer_placement_body_entered(body:Node2D) -> void:
+	bouncer_is_colliding = true
