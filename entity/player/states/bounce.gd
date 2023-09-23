33a813b4cdf5ee5339d5039172303cf027b4feec
Jay Mehta Laptop <wjayww+asus_laptop@gmail.com>
2023-09-23 13:28:47 -0500

    dont end bounce if on floor

--- entity/player/states/bounce.gd
+++ entity/player/states/bounce.gd
@@ -4,12 +4,15 @@ var player: Player
 var stats: PlayerStats
 
 func state_process_physics(delta: float) -> void:
-	if player.is_on_floor() and player.velocity.y >= 0:
-		transition_state("move")
+	# if player.is_on_floor() and player.velocity.y >= 0:
+	# 	transition_state("move")
 	player.velocity.y += stats.gravity_accel * delta
 	if absf(player.velocity.x) < stats.move_speed * 0.1: # arbitrary condition
 		transition_state("move")
-	player.velocity.x -= player.velocity.x * 0.5 * delta
+	if player.is_on_floor():
+		player.velocity.x -= player.velocity.x * 2 * delta
+	else:
+		player.velocity.x -= player.velocity.x * 1 * delta
 
 	player.move_and_slide()
 
