bb9b3c8d25ab00f4c50521e49a104b3007110c64
Banana-Cultist <50812299+Banana-Cultist@users.noreply.github.com>
2023-09-24 13:01:15 -0500

    Coin Call + Money Boon

--- game/game.gd
+++ game/game.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
+class_name GameScene
+
+static var player_1_boon: bool
+
+@onready var player_1: Player = $"Player1"
+@onready var player_2: Player = $"Player2"
+
 func _ready():
 	var music: AudioStreamPlayer# = $"AudioStreamPlayer"
 	if music:
 		music.finished.connect(func(): music.playing = true)
 	
+	if player_1_boon:
+		player_1.coin_count += 5
+	else:
+		player_2.coin_count += 5
+	
 	# more debugging crap:
-	$"Shop Item".init(TrapType.Types.TRAMPOLINE)
-
+	#$"Shop Item".init(TrapType.Types.TRAMPOLINE)
