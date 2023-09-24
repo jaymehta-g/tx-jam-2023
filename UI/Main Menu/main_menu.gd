4d711b4b5198e8b42a12a0c7dd307b6772b85a90
Banana-Cultist <50812299+Banana-Cultist@users.noreply.github.com>
2023-09-24 00:49:43 -0500

    Added Coin Flip

--- UI/Main Menu/main_menu.gd
+++ UI/Main Menu/main_menu.gd
@@ -1,13 +1,40 @@
 extends Node
 
+var coin: AnimatedSprite2D
+var coin_velocity: float = 0
+var gravity: float = -1.0 / 8
+var coin_launch: float = -25.0 / 5
+var coin_active = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if coin_active:
+		coin.global_position.y += coin_velocity
+		if abs(coin_velocity) < 10:
+			coin_velocity -= gravity / 5
+		else:
+			coin_velocity -= gravity
+		
+		# Coin has landed
+		if coin_velocity >= -coin_launch:
+			coin_active = false
+			coin.stop()
+			if randi() % 2 == 0:
+				coin.frame = 3 # Heads
+			else:
+				coin.frame = 9 # Tails
+			
+
 
 func _on_texture_button_pressed():
-	get_tree().change_scene_to_packed(ResourceManager.GAME_SCENE)
+	$"CanvasLayer/MarginContainer/CenterContainer/TextureButton".visible = false
+	coin = $"CanvasLayer/Coin"
+	coin.play("Flipping")
+	coin_active = true
+	coin_velocity = coin_launch
+	#get_tree().change_scene_to_packed(ResourceManager.GAME_SCENE)
 	pass # Replace with function body.
--- misc/resource_manager.gd
+++ misc/resource_manager.gd
@@ -4,6 +4,6 @@ var BUMPER_SCENE = preload("res://entity/bouncer/bouncer.tscn")
 var TRAMPOLINE_SCENE = preload("res://entity/trampoline/trampoline.tscn")
 var PROPELLOR_SCENE = preload("res://entity/propellor/propellor.tscn")
 
-var SMOKE_TRAIL_SCENE = preload("res://entity/player/smoke_fx/flying_smoke_particles.tscn")
+#r SMOKE_TRAIL_SCENE = preload("res://entity/player/smoke_fx/flying_smoke_particles.tscn")
 
 var GAME_SCENE = preload("res://game/game.tscn")
