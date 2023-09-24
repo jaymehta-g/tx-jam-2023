0b1ee37ba8f47e4a8d021487a5a4176c7e61704e
Jay Mehta Laptop <wjayww+asus_laptop@gmail.com>
2023-09-23 20:17:58 -0500

    game scene in res manager

--- misc/resource_manager.gd
+++ misc/resource_manager.gd
@@ -2,5 +2,8 @@ extends Node
 
 var BUMPER_SCENE = preload("res://entity/bouncer/bouncer.tscn")
 var TRAMPOLINE_SCENE = preload("res://entity/trampoline/trampoline.tscn")
+var PROPELLOR_SCENE = preload("res://entity/propellor/propellor.tscn")
 
 var SMOKE_TRAIL_SCENE = preload("res://entity/player/smoke_fx/flying_smoke_particles.tscn")
+
+var GAME_SCENE = preload("res://game/game.tscn")
