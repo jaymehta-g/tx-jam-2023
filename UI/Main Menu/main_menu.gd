4497f1ab92cf04a151141a84b71b26d93ee70b9d
Banana-Cultist <50812299+Banana-Cultist@users.noreply.github.com>
2023-09-24 11:31:14 -0500

    Music

--- UI/Main Menu/main_menu.gd
+++ UI/Main Menu/main_menu.gd
@@ -6,9 +6,16 @@ var gravity: float = -1.0 / 15
 var coin_launch: float = -21.0 / 3
 var coin_active = false
 
+var music: AudioStreamPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	music = $"AudioStreamPlayer"
+	music.finished.connect(replay.bind(music))
 	pass
+
+func replay(music: AudioStreamPlayer):
+	music.playing = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -36,6 +43,8 @@ func _on_texture_button_pressed():
 	$"CanvasLayer/MarginContainer/CenterContainer/TextureButton".visible = false
 	coin = $"CanvasLayer/Coin"
 	$"Initial Wait".start()
+	music.finished.disconnect(replay.bind(music))
+	music.playing = false
 	pass # Replace with function body.
 
 
--- game/game.gd
+++ game/game.gd
@@ -1,1 +1,6 @@
 extends Node2D
+
+func _ready():
+	var music: AudioStreamPlayer = $"AudioStreamPlayer"
+	music.finished.connect(func(): music.playing = true)
+
