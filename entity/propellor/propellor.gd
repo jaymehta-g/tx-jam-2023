0ea7be0eab65591635978391fa9fbdf627599592
Jay Mehta Laptop <wjayww+asus_laptop@gmail.com>
2023-09-24 17:14:35 -0500

    propellor "fix"

--- entity/propellor/propellor.gd
+++ entity/propellor/propellor.gd
@@ -3,18 +3,28 @@ extends Node2D
 @export var strength: float
 @export var coll_shape: CollisionShape2D
 
+@export var particles_scene: PackedScene
+
 var facing_multiplier: float
+var particles: GPUParticles2D
 
 func _ready() -> void:
+	if OS.get_name() == "Windows":
+		var node = particles_scene.instantiate()
+		add_child(node)
+		particles = node
 	$"Timer".timeout.connect(func(): 
-		$"GPUParticles2D".emitting = false
+		if particles:
+			particles.emitting = false
 		coll_shape.queue_free()
 		$"Wait To Erase Timer".start()
 	)
 
+
+
 func init(player):
-	var particles: GPUParticles2D = $"GPUParticles2D"
-	particles.process_material.gravity.x *= player.facing_right_multiplier
+	if particles:
+		particles.process_material.gravity.x *= player.facing_right_multiplier
 	facing_multiplier = player.facing_right_multiplier
 
 func _on_area_2d_body_entered(body:Node2D) -> void:
