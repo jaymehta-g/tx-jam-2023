51604837f3af2cb795ea962cc09de8767fb0517c
Jay Mehta Laptop <wjayww+asus_laptop@gmail.com>
2023-09-23 19:13:49 -0500

    particles dont work

--- entity/player/states/bounce.gd
+++ entity/player/states/bounce.gd
@@ -5,42 +5,52 @@ var stats: PlayerStats
 
 @export var right_raycast: RayCast2D
 @export var left_raycast: RayCast2D
+@export var remote_transform: RemoteTransform2D
+
+var fly_particles: GPUParticles2D
+
+var new_velocity: Vector2
 
 func state_process_physics(delta: float) -> void:
-	# player.velocity.y += stats.gravity_accel * delta
-	# if absf(player.velocity.x) < stats.move_speed * 0.5: # arbitrary condition
-	# 	transition_state("move")
-	# if player.is_on_floor():
-	# 	player.velocity.x -= player.velocity.x * 2 * delta
-	# else:
-	# 	player.velocity.x -= player.velocity.x * 1 * delta
+	const BOUNCE_DAMP := 0.7 # TODO move to stats
 
-	const BOUNCE_DAMP := 1.0 # TODO move to stats
 
-	player.velocity.y += stats.gravity_accel * delta
-	if player.is_on_floor() and player.velocity.length() < 100: # magic number must fix
+	new_velocity.y += stats.gravity_accel * delta
+	if new_velocity.length() < 100: # magic number must fix
+		player.velocity = new_velocity
 		transition_state("move")
 	
 	# bounce around
-	if player.is_on_floor() and player.velocity.y > 0:
-		player.velocity.y = -absf(player.velocity.y) * BOUNCE_DAMP
-	if player.is_on_ceiling() and player.velocity.y < 0:
-		player.velocity.y = absf(player.velocity.y) * BOUNCE_DAMP
-		print_debug("bounce ceil")
+	if player.is_on_floor() and new_velocity.y > 0:
+		new_velocity.y = -absf(new_velocity.y) * BOUNCE_DAMP
+	if player.is_on_ceiling() and new_velocity.y < 0:
+		new_velocity.y = absf(new_velocity.y) * BOUNCE_DAMP
 	if player.is_on_wall():
-		var abs_x_vel := absf(player.velocity.x)
-		if right_raycast.is_colliding() and player.velocity.x > 0:
-			player.velocity.x = -abs_x_vel * BOUNCE_DAMP
-		elif left_raycast.is_colliding() and player.velocity.x < 0:
-			player.velocity.x = abs_x_vel * BOUNCE_DAMP
+		var abs_x_vel := absf(new_velocity.x)
+		if right_raycast.is_colliding() and new_velocity.x > 0:
+			new_velocity.x = -abs_x_vel * BOUNCE_DAMP
+		elif left_raycast.is_colliding() and new_velocity.x < 0:
+			new_velocity.x = abs_x_vel * BOUNCE_DAMP
 
-	
+	# horizontal damping so you can come to a stop
+	new_velocity.x -= new_velocity.x * 2 * delta
+
+	player.velocity = new_velocity
 	player.move_and_slide()
 
+	# juice
+	const EMISSION_BY_SPEED := 4
+	var particles_speed := new_velocity.length()
+	fly_particles.amount = floori(EMISSION_BY_SPEED*particles_speed)
+
 func state_enter() -> void:
-	print_debug("bounce enter")
 	player = base
 	stats = player.stats
+	new_velocity = player.velocity
+	fly_particles = ResourceManager.SMOKE_TRAIL_SCENE.instantiate()
+	fly_particles.set_emitting(true)
+	player.add_to_level.emit(fly_particles)
+	remote_transform.remote_path = fly_particles.get_path()
 
 func state_exit() -> void:
-	print_debug("state exit")
+	fly_particles.queue_free()
--- misc/resource_manager.gd
+++ misc/resource_manager.gd
@@ -2,3 +2,5 @@ extends Node
 
 var BUMPER_SCENE = preload("res://entity/bouncer/bouncer.tscn")
 var TRAMPOLINE_SCENE = preload("res://entity/trampoline/trampoline.tscn")
+
+var SMOKE_TRAIL_SCENE = preload("res://entity/player/smoke_fx/flying_smoke_particles.tscn")
