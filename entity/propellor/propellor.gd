533fa526325f5a791495f2dd194be33e7d124efd
Banana-Cultist <50812299+Banana-Cultist@users.noreply.github.com>
2023-09-24 17:31:26 -0500

    Propeller Animation Done

--- entity/propellor/propellor.gd
+++ entity/propellor/propellor.gd
@@ -5,6 +5,8 @@ extends Node2D
 
 @export var particles_scene: PackedScene
 
+@onready var propeller_animation: AnimatedSprite2D = $"AnimatedSprite2D"
+
 var facing_multiplier: float
 var particles: GPUParticles2D
 
@@ -19,8 +21,9 @@ func _ready() -> void:
 		coll_shape.queue_free()
 		$"Wait To Erase Timer".start()
 	)
-
-
+	
+	propeller_animation.play("start-up")
+	propeller_animation.animation_finished.connect(func(): propeller_animation.play("spinning"))
 
 func init(player):
 	if particles:
@@ -34,3 +37,9 @@ func _on_area_2d_body_entered(body:Node2D) -> void:
 	if player.current_state.name.to_lower() == "dash":
 		return
 	player.bounce(Vector2.RIGHT.rotated(-0.2) * facing_multiplier, strength)
+
+
+func _on_wait_to_erase_timer_timeout():
+	propeller_animation.play("end")
+	propeller_animation.animation_finished.connect(func(): queue_free())
+	pass # Replace with function body.
